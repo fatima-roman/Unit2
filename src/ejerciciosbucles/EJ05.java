a9e743f407664846c677ff1b42898f5c1a5d5054
fatima.roman <fatima.roman@106-04.info2>
2025-11-17 10:18:23 +0100

    con espacion

--- src/ejerciciosbucles/EJ05.java
+++ src/ejerciciosbucles/EJ05.java
@@ -19,14 +19,29 @@ public class EJ05 {
 			System.out.println();
 		}
 		
+		System.out.println();
 		
-		for (int i = 1; i<=n; i++) {
+		/*for (int i = 1; i<=n; i++) {
 			for (int k = 1; k<=i; k++) {
 				System.out.print("*");
 			}
 			System.out.println();
-		}
+		}*/
+		
+		for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
 
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+                if (j < i) {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
 		sc.close();
 	}
 
